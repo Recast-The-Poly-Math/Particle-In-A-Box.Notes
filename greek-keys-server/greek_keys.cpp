5e21a9746a0f8b53b1c4f858df50d526c2d6f0f3
Recast-The-Poly-Math <theexpletition@gmail.com>
2026-08-15 19:27:11 -0500

    Add Greek Keys C++ file generator

---
+++ greek-keys-server/greek_keys.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string line;
+    cout << "GREEK KEYS\n============\n\n";
+    while(getline(cin,line)) cout << line << '\n';
+    return 0;
+}
